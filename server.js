ed16bb5ab3076e2b43cba5fed27ad7acb990054c
Dennis Ruto <rutodeno@gmail.com>
2018-06-07 20:52:10 -0400

    requried dependacies

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
